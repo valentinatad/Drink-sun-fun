8ca8661ae841f17c32fc0187d992439797465191
Valentina Tadic <valentina.tadic008@gmail.com>
2020-04-03 14:47:32 +0200

    Updates

---
+++ precache-manifest.d368cf42f9f360a6c02fed5a59b7bc29.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d0c1b5152b4dc1cecedf1012196bcb",
+    "url": "/Zavrsni-Projekat1/index.html"
+  },
+  {
+    "revision": "8d0c67c868647f0dd9bd",
+    "url": "/Zavrsni-Projekat1/static/css/2.6bbb3a6b.chunk.css"
+  },
+  {
+    "revision": "d66a0c51b0b2468fdf00",
+    "url": "/Zavrsni-Projekat1/static/css/main.24cb0f71.chunk.css"
+  },
+  {
+    "revision": "8d0c67c868647f0dd9bd",
+    "url": "/Zavrsni-Projekat1/static/js/2.a5a44b09.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/Zavrsni-Projekat1/static/js/2.a5a44b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66a0c51b0b2468fdf00",
+    "url": "/Zavrsni-Projekat1/static/js/main.a9d0f5c6.chunk.js"
+  },
+  {
+    "revision": "b18ec99eb65f1518bba7",
+    "url": "/Zavrsni-Projekat1/static/js/runtime-main.bfaff9a4.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Zavrsni-Projekat1/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Zavrsni-Projekat1/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Zavrsni-Projekat1/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Zavrsni-Projekat1/static/media/slick.f97e3bbf.svg"
+  }
+]);
